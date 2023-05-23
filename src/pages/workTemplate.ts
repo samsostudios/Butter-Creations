b3afcec46b5ff73696d00562455a3dd830919510
samsostudios <sam@samsostudios.com>
2023-05-22 22:39:36 -0600

    work template video aspect ratio adjust

--- src/pages/workTemplate.ts
+++ src/pages/workTemplate.ts
@@ -2,4 +2,10 @@ export const workTemplate = () => {
   // Page Globals
   // ------------
   // console.log('work template');
+  const mainVideo = document.querySelector('.vimeo-embed_container') as HTMLElement;
+  const mainVideoAspect = document.querySelector('.work-hero_aspect-ratio')?.innerHTML;
+
+  if (mainVideoAspect === '21:9') {
+    mainVideo.style.paddingBottom = '42.85%';
+  }
 };
